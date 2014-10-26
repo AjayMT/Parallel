2a2ccff6cb6a81ae0acd8b65f71e3c1638061765
Ajay MT <ajay.tatachar@gmail.com>
2014-10-26 12:44:02 +0530

    Initial Commit

---
+++ Parallel/Parallel.h
@@ -0,0 +1,13 @@
+//
+//  Parallel.h
+//  Parallel
+//
+//  Created by Ajay Madhusudan on 26/10/14.
+//  Copyright (c) 2014 Ajay Madhusudan. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Parallel : NSObject
+
+@end
---
+++ Parallel/Parallel.m
@@ -0,0 +1,13 @@
+//
+//  Parallel.m
+//  Parallel
+//
+//  Created by Ajay Madhusudan on 26/10/14.
+//  Copyright (c) 2014 Ajay Madhusudan. All rights reserved.
+//
+
+#import "Parallel.h"
+
+@implementation Parallel
+
+@end
